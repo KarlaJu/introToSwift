6b9c6ae51043d96b64f59ffad07b8cbb4ec990e5
KarlaJu <karla@makingdevs.com>
2019-10-19 11:22:41 -0500

    Tipos de variables

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,36 @@
 import Cocoa
 
-var str = "Hello, playground"
+//variables
+var unEntero: Int
+
+unEntero = 10
+
+//Variable inicializada
+var otroEntero: Int = 8
+
+// mas información en la barra, aún más en el toogle y aún má en el ojo
+//El compilador trata de leer de que tipo es la variable
+
+var segundoEntero = 8
+// el compilador ni siquiera va a compilarlo: segundoEntero = "asda"
+//Una vez defino el tipo de la variable no se puede cambiar
+
+
+//Constantes
+//Cuando se pone let, una vez que la varialbe tenga let ya no puede cambiar el valor de la variable
+let unaConstante = 8
+
+//-------Una recomendación es que se utilice más bien let siempre, hay casos en los que se necesita usar var pero es mejor usar let
+
+//Cadenas
+let cadena: String = "Mi cadena"
+let unaCadena = "Una cadena"
+//Tiene soporte de unicode
+let cadena2 = "Mi ca\u{301}dena"
+//También los entiende de esta manera
+let cadena3 = "Mi cádena"
+
+//Tiene autocompletado
+cadena.count
+cadena2.count
+
